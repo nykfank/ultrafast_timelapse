e44ca073cd1903bd7fa2132fd503eab0889217d1
nykfank <nick@nyk.ch>
2020-11-07 01:13:39 +0100

    Update image_histogram_distance_rgb.py

--- image_histogram_distance_rgb.py
+++ image_histogram_distance_rgb.py
@@ -1,40 +1,26 @@
 #!/usr/bin/python
-import Image,os,sys,string,time,ImageDraw,time,ImageFont
-startDir=os.path.abspath(sys.argv[1]).rstrip('/')
-getTime=lambda x : int(filter(lambda y : y in string.digits,os.path.basename(x)))
+import os, sqlite3, sys
+f1 = sys.argv[1]
+f2 = sys.argv[2]
 
-def load_font(fontSize):
- """Load the arial TTF font from the default location for gentoo and debian linux"""
- f1='/usr/share/fonts/corefonts/arialbd.ttf' 
- f2='/usr/share/fonts/truetype/msttcorefonts/Arial_Bold.ttf'
- if os.path.isfile(f1): font=ImageFont.truetype(f1,fontSize)
- if os.path.isfile(f2): font=ImageFont.truetype(f2,fontSize)
- return font
-
-def draw_date(img,txs):
- """Write date and time in the image"""
- fontSize=img.size[1]/20
- draw = ImageDraw.Draw(img) # new draw instance 
- arial=load_font(fontSize)
- tsize=draw.textsize(txs,font=arial)
- #draw.text((img.size[0]-tsize[0]-10,img.size[1]-(fontSize+5)),txs,fill=(200,200,255),font=arial)
- draw.text((10,img.size[1]-(fontSize+5)),txs,fill=(50,75,255),font=arial)
- return img
-
-start_time=time.time()
-is_jpeg=lambda x : x.endswith('.jpg')
-photos=sorted(filter(is_jpeg,os.listdir(startDir)))
-if '_' in photos[0]: getTime=lambda x : int(time.mktime(time.strptime(x, '%Y%m%d_%H%M%S')))
-else: getTime=lambda x : int(filter(lambda y : y in string.digits,os.path.basename(x)))
-for f in photos:
- fn='%s/%s' % (startDir,f)
- t=getTime(os.path.splitext(f)[0])
- #txs=time.strftime('%Y-%m-%d %H:%M',time.gmtime(t))
- txs=time.strftime('%Y-%m',time.localtime(t))
- i=Image.open(fn)
- i2=draw_date(i,txs)
- i2.save(fn)
-total_time=time.time() - start_time
-print 'date_to_timelapse: %d images in %f seconds' % (len(photos),total_time)
-
- 
+cache_fn='%s/.hist_dist_cache.db' % os.getenv("HOME")
+db = sqlite3.connect(cache_fn)
+dbc = db.cursor()
+dbc.execute('CREATE TABLE IF NOT EXISTS distances (f1 TEXT, f2 TEXT, dist FLOAT)')
+dbc.execute('CREATE INDEX IF NOT EXISTS distances_idx ON distances (f1, f2)')
+dbc.execute('SELECT dist FROM distances WHERE f1 = ? AND f2 = ?',(f1, f2))
+row = dbc.fetchone()
+if row: 
+	print row[0]
+else:
+	import cv2
+	im1 = cv2.imread(f1)
+	im2 = cv2.imread(f2)
+	distlist = []
+	for channel in range(3):
+		hist1 = cv2.calcHist([im1],[channel],None,[256],[0,256])
+		hist2 = cv2.calcHist([im2],[channel],None,[256],[0,256])
+		distlist.append( cv2.compareHist(hist1,hist2,cv2.HISTCMP_BHATTACHARYYA) )
+	dbc.execute('INSERT INTO distances (f1, f2, dist) VALUES (?,?,?)',(f1, f2, sum(distlist)))
+	db.commit()
+	print sum(distlist)
