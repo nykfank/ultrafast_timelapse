4635fdffa90d388c8be5b37d6ca1844c9d468207
nykfank <nick@nyk.ch>
2020-11-08 00:32:47 +0100

    Update date_to_timelapse_monyear.py

--- date_to_timelapse_monyear.py
+++ date_to_timelapse_monyear.py
@@ -24,7 +24,7 @@ def draw_date(img,txs):
 start_time=time.time()
 is_jpeg=lambda x : x.endswith('.jpg')
 photos=sorted(filter(is_jpeg,os.listdir(startDir)))
-if '_' in photos[0]: getTime=lambda x : int(time.mktime(time.strptime(x, '%Y%m%d_%H%M%S')))
+if '_' in photos[0]: getTime=lambda x : int(time.mktime(time.strptime(x.replace('D', ''), '%Y%m%d_%H%M%S')))
 else: getTime=lambda x : int(filter(lambda y : y in string.digits,os.path.basename(x)))
 for f in photos:
  fn='%s/%s' % (startDir,f)
